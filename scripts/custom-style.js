bb46f6d69d34231407f084731f6df4286d30554f
Sumu154 <sumaiyatasnim5548@gmail.com>
2024-09-24 10:12:26 +0600

    all set and nav bar completed

---
+++ scripts/custom-style.js
@@ -0,0 +1,20 @@
+tailwind.config = {
+    theme: {
+      extend: {
+
+        fontFamily: {
+          'Lexend': ['Lexend', 'sans-serif'],
+        },
+        
+        colors: {
+          lightgreen: '#B4F461',
+          lightash: '#F9F7F3',
+          black: '#111111',
+        },
+
+        backgroundImage: {
+          'bginput': "url('assets/image/bg.jpg')",
+        }
+      }
+    }
+  }
