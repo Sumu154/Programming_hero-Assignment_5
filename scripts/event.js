b5ff6b3f1d984e85cae05a0ff5add3bfd28cad2b
Sumu154 <sumaiyatasnim5548@gmail.com>
2024-09-24 22:39:52 +0600

    add money is done for noakhali portion

--- scripts/event.js
+++ scripts/event.js
@@ -10,3 +10,20 @@ document.getElementById('donation-btn').addEventListener('click', function(){
     document.getElementById('history-div').classList.add('hidden');
 
 })
+
+
+  // add donation money 
+document.getElementById('noakhali-donate').addEventListener('click', function(event){
+    event.preventDefault();
+    const addMoney = parseFloat(document.getElementById('noakhali-add').value);      // input er money string ke number e nite hbe
+    if(addMoney > 0){
+        const presentMoney = parseFloat(document.getElementById('noakhali-money').innerText);
+        const totalMoney = parseFloat(document.getElementById('total-money').innerText);
+
+        document.getElementById('noakhali-money').innerText = presentMoney + addMoney; 
+
+    }
+    else{
+        alert('Failed to add Money.Please try again.')
+    }
+})
